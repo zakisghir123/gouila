4d266c7fb937971ec7749f8c5bba1361a8271862
zakisghir123 <57421107+zakisghir123@users.noreply.github.com>
2019-11-06 01:53:58 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 ﻿const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "%";
+var prefix = "$";
 client.on("message", message => {
 
             if (message.content.startsWith(prefix + "bc")) {
